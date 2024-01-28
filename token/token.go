398112649c57b92bf2884edb2909e6f4e2afbe72
csaw101 <un1xc0rn@proton.me>
2024-01-28 12:16:22 +0200

    formatting

--- token/token.go
+++ token/token.go
@@ -3,64 +3,64 @@ package token
 type TokenType string
 
 type Token struct {
-    Type TokenType
-    Literal string
+	Type    TokenType
+	Literal string
 }
 
 const (
-    ILLEGAL = "ILLEGAL"
-    EOF = "EOF"
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
 
-    // Identifiers + literals
-    IDENT = "IDENT"
-    INT = "INT"
+	// Identifiers + literals
+	IDENT = "IDENT"
+	INT   = "INT"
 
-    // Operators
-    ASSIGN = "="
-    PLUS = "+"
-    MINUS = "-"
-    BANG = "!"
-    ASTERISK = "*"
-    SLASH = "/"
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
 
-    LT = "<"
-    GT = ">"
-    EQ = "=="
-    NOT_EQ = "!="
+	LT     = "<"
+	GT     = ">"
+	EQ     = "=="
+	NOT_EQ = "!="
 
-    // Delimiters
-    COMMA = ","
-    SEMICOLON = ":"
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ":"
 
-    LPAREN = "("
-    RPAREN = ")"
-    LBRACE = "{"
-    RBRACE = "}"
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
 
-    // Keywords
-    FUNCTION = "FUNCTION"
-    LET = "LET"
-    TRUE = "TRUE"
-    FALSE = "FALSE"
-    IF = "IF"
-    ELSE = "ELSE"
-    RETURN = "RETURN"
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 var Keywords = map[string]TokenType{
-    "fn": FUNCTION,
-    "let": LET,
-    "true": TRUE,
-    "false": FALSE,
-    "if": IF,
-    "else": ELSE,
-    "return": RETURN,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
-    if tok, ok := Keywords[ident]; ok {
-        return tok
-    }
+	if tok, ok := Keywords[ident]; ok {
+		return tok
+	}
 
-    return IDENT
+	return IDENT
 }
